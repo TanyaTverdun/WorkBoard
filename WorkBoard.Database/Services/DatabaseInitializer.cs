786f8ee7708ce14813df5ab31bac9cb7d07e37e3
Tanya Tverdun <tanyatverdun@gmail.com>
2026-05-27 16:54:52 +0300

    feat: create services to initialize db and start migration

---
+++ WorkBoard.Database/Services/DatabaseInitializer.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using DbUp;
+
+namespace WorkBoard.Database.Services;
+
+public static class DatabaseInitializer
+{
+    public static void Initialize(string connectionString)
+    {
+        EnsureDatabase.For.SqlDatabase(connectionString);
+
+        var upgrader = DeployChanges.To
+            .SqlDatabase(connectionString)
+            .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+            .LogToConsole()
+            .Build();
+
+        var result = upgrader.PerformUpgrade();
+
+        if (!result.Successful)
+        {
+            throw new DataMisalignedException(
+                "Database migration faild during DbUp execution", 
+                result.Error);
+        }
+    }
+}
---
+++ WorkBoard.Database/Services/MigrationRunner.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using WorkBoard.Database.Factories;
+
+namespace WorkBoard.Database.Services;
+
+public class MigrationRunner
+{
+    private readonly ILogger<MigrationRunner> _logger;
+    private const string ConnectionStringKey = "DefaultConnection";
+    private const string AppSettingsFileName = "appsettings.json";
+
+    public MigrationRunner(ILogger<MigrationRunner> logger)
+    {
+        this._logger = logger;
+    }
+
+    public int Run()
+    {
+        this._logger.LogInformation("Starting database migration");
+
+        var configuration = InfrastructureFactory
+            .BuildConfiguration(AppSettingsFileName);
+        var connectionString = configuration
+            .GetConnectionString(ConnectionStringKey);
+
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            this._logger.LogError(
+                $"Connection string '{ConnectionStringKey}' " +
+                $"is missing in {AppSettingsFileName}.");
+            return -1;
+        }
+
+        DatabaseInitializer.Initialize(connectionString);
+
+        this._logger.LogInformation("Database migrations completed successfully!");
+
+        return 0;
+    }
+}
