efc86b332bd259f45ddeaa5ed1ccdc8085e58d8b
Tanya Tverdun <tanyatverdun@gmail.com>
2026-05-27 16:42:03 +0300

    feat: add factory class to create configuration and logger

---
+++ WorkBoard.Database/Factories/InfrastructureFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WorkBoard.Database.Factories;
+
+public static class InfrastructureFactory
+{
+    public static IConfiguration BuildConfiguration(string fileName)
+    {
+        return new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(
+                fileName, 
+                optional: false, 
+                reloadOnChange: true)
+            .Build();
+    }
+
+    public static ILoggerFactory CreateLoggerFactory()
+    {
+        return LoggerFactory.Create(builder =>
+        {
+            builder
+                .AddConsole()
+                .SetMinimumLevel(LogLevel.Information);
+        });
+    }
+}
