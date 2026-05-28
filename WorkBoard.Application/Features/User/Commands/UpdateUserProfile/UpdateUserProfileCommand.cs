86d9ed83a3f4a93a5923a6b343ecb2c04596e5dc
Tanya Tverdun <tanyatverdun@gmail.com>
2026-05-28 22:16:35 +0300

    feat: create command for updating user profile

---
+++ WorkBoard.Application/Features/User/Commands/UpdateUserProfile/UpdateUserProfileCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace WorkBoard.Application.Features.User.Commands.UpdateUserProfile;
+
+/// <summary>
+/// Command data structure dispatched from the API layer.
+/// </summary>
+public record UpdateUserProfileCommand(
+    Guid UserId,
+    string? FullName,
+    string? AvatarUrl) : IRequest<bool>;
