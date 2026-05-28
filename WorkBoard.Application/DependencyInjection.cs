029337f37a9de9b0e22859d94a4d25689c1f3e29
Tanya Tverdun <tanyatverdun@gmail.com>
2026-05-28 22:21:09 +0300

    feat: register MediatR and Mapping into DI container

---
+++ WorkBoard.Application/DependencyInjection.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace WorkBoard.Application;
+
+/// <summary>
+/// Extension methods to register application-layer services 
+/// into the dependency injection container
+/// </summary>
+public static class DependencyInjection
+{
+    /// <summary>
+    /// Registers application services
+    /// </summary>
+    /// <param name="services">
+    /// The <see cref="IServiceCollection"/> to add the services
+    /// </param>
+    /// <returns>
+    /// The same <see cref="IServiceCollection"/> instance
+    /// </returns>
+    public static IServiceCollection AddApplication(
+        this IServiceCollection services)
+    {
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+
+        services.AddAutoMapper(cfg =>
+        {
+            cfg.AddMaps(Assembly.GetExecutingAssembly());
+        });
+
+        return services;
+    }
+}
