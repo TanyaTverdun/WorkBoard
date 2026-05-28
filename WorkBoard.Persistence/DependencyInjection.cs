6c55b5910bf466ed2d9a178d4e963f779dd1c9cf
Tanya Tverdun <tanyatverdun@gmail.com>
2026-05-28 16:00:18 +0300

    feat: implement database connection factory
    - Add IDbConnectionFactory interface and SQL Server implementation
    - Implement Dispose pattern
    - Register DbConnectionFactory as scoped service in DI container

---
+++ WorkBoard.Persistence/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WorkBoard.Application.Common.Interfaces;
+
+namespace WorkBoard.Persistence;
+
+/// <summary>
+/// Extension methods for registering persistence 
+/// layer services into DI container
+/// </summary>
+public static class DependencyInjection
+{
+
+    /// <summary>
+    /// Registers persistence services
+    /// </summary>
+    /// <param name="services">
+    /// The <see cref="IServiceCollection"/> to add services
+    /// </param>
+    /// <returns>
+    /// The same service collection
+    /// </returns>
+    public static IServiceCollection AddPersistance(
+        this IServiceCollection services)
+    {
+        services.AddScoped<IDbConnectionFactory, IDbConnectionFactory>();
+
+        return services;
+    }
+}
