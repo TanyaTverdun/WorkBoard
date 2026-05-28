e1efd993223eb98d73a5237148a90ebbc3dde08a
Tanya Tverdun <tanyatverdun@gmail.com>
2026-05-28 12:30:07 +0300

    feat: add interface for unit of work

---
+++ WorkBoard.Application/Common/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿namespace WorkBoard.Application.Common.Interfaces;
+
+/// <summary>
+/// Maintance database tranzactions
+/// </summary>
+public interface IUnitOfWork : IDisposable
+{
+    /// <summary>
+    /// Commit all changes made in current tranzaction to database
+    /// </summary>
+    /// <param name="cancellationToken">
+    /// To cancel operation
+    /// </param>
+    /// <returns>
+    /// A Task that represents the asynchronous operation
+    /// </returns>
+    Task CommitAsync(CancellationToken cancellationToken = default);
+}
