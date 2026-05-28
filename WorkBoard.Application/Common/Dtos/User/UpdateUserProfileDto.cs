579106c184d148b2a37bdb0db7019be1536f15ba
Tanya Tverdun <tanyatverdun@gmail.com>
2026-05-28 22:18:00 +0300

    feat: create DTOs to represent user profile and update information about profile

---
+++ WorkBoard.Application/Common/Dtos/User/UpdateUserProfileDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WorkBoard.Application.Common.Dtos.User;
+
+/// <summary>
+/// Data transfer object containing the profile changes sent from the UI
+/// </summary>
+public record UpdateUserProfileDto(
+    string? FullName,
+    string? AvatarUrl);
---
+++ WorkBoard.Application/Common/Dtos/User/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkBoard.Application.Common.Dtos.User;
+
+/// <summary>
+/// A data transfer object representing user information
+/// </summary>
+public record UserDto(
+    Guid UserId,
+    string? FullName,
+    string Email,
+    string? AvatarUrl);
