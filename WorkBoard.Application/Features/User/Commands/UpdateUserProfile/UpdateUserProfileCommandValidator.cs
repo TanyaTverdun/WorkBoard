635070d180850e0d65a9a8b446bc0a6e71ae3ee9
Tanya Tverdun <tanyatverdun@gmail.com>
2026-05-31 22:29:55 +0300

    feat: add validator for user profile update

---
+++ WorkBoard.Application/Features/User/Commands/UpdateUserProfile/UpdateUserProfileCommandValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+
+namespace WorkBoard.Application.Features.User.Commands.UpdateUserProfile;
+
+/// <summary>
+/// Provides validation rules for the 
+/// <see cref="UpdateUserProfileCommand"/>.
+/// </summary>
+public class UpdateUserProfileCommandValidator 
+    : AbstractValidator<UpdateUserProfileCommand>
+{
+    /// <summary>
+    /// Initializes a new instance of the 
+    /// <see cref="UpdateUserProfileCommandValidator"/> class and defines rules
+    /// </summary>
+    public UpdateUserProfileCommandValidator()
+    {
+        RuleFor(v => v.UserId)
+            .NotEmpty()
+            .WithMessage("User ID is required.");
+
+        RuleFor(v => v.FullName)
+            .MaximumLength(100)
+            .WithMessage("Full Name must not exceed 100 characters.");
+
+        RuleFor(v => v.AvatarUrl)
+            .MaximumLength(2048)
+            .WithMessage("Avatar URL must not exceed 2048 characters.");
+    }
+}
