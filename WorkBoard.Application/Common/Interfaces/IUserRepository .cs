3162cfb396de38efd6663ccaf1342d6b175d2dae
Tanya Tverdun <tanyatverdun@gmail.com>
2026-06-02 19:27:27 +0300

    fix: delete unnecessary contract methods in IUserRepository

--- WorkBoard.Application/Common/Interfaces/IUserRepository .cs
+++ WorkBoard.Application/Common/Interfaces/IUserRepository .cs
@@ -5,12 +5,4 @@ namespace WorkBoard.Application.Common.Interfaces;
 
 public interface IUserRepository : IGenericRepository<User, Guid>
 {
-    Task<User?> GetByIdAsync(
-        Guid userId,
-        CancellationToken cancellationToken = default);
-
-    Task<bool> AddAsync(
-        User user,
-        IDbTransaction? transaction = null,
-        CancellationToken cancellationToken = default);
 }
