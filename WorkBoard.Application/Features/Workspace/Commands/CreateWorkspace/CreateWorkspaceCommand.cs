bc43b353499bc6cc13ed675ac2712c1120b5f4c5
Tanya Tverdun <tanyatverdun@gmail.com>
2026-06-08 20:32:29 +0300

    feat: implement CreateWorkspace command, validator and handler

---
+++ WorkBoard.Application/Features/Workspace/Commands/CreateWorkspace/CreateWorkspaceCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace WorkBoard.Application.Features.Workspace.Commands.CreateWorkspace;
+
+public class CreateWorkspaceCommand : IRequest<Guid>
+{
+    public required string Name { get; set; }
+}
---
+++ WorkBoard.Application/Features/Workspace/Commands/CreateWorkspace/CreateWorkspaceCommandHandler.cs
@@ -0,0 +1,71 @@
+﻿using MediatR;
+using WorkBoard.Application.Common.Interfaces;
+using WorkBoard.Domain.Entities;
+using WorkBoard.Domain.Enums;
+using WorkspaceEntity = WorkBoard.Domain.Entities.Workspace;
+
+namespace WorkBoard.Application.Features.Workspace.Commands.CreateWorkspace;
+
+public class CreateWorkspaceCommandHandler : 
+    IRequestHandler<CreateWorkspaceCommand, Guid>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+    private readonly IUserContext _userContext;
+
+    public CreateWorkspaceCommandHandler(
+        IUnitOfWorkFactory unitOfWorkFactory,
+        IUserContext userContext)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+        _userContext = userContext;
+    }
+
+    public async Task<Guid> Handle(
+        CreateWorkspaceCommand request,
+        CancellationToken cancellationToken)
+    {
+        var currentUserId = _userContext.UserId
+            ?? throw new UnauthorizedAccessException(
+                "User is not authenticated.");
+
+        var workspaceId = Guid.NewGuid();
+
+        var workspace = new WorkspaceEntity
+        {
+            Id = workspaceId,
+            Name = request.Name,
+            SubscriptionTier = SubscriptionTier.Free,
+            CreatedAt = DateTime.UtcNow,
+            CreatedBy = currentUserId
+        };
+
+        var workspaceMember = new WorkspaceMember
+        {
+            UserId = currentUserId,
+            WorkspaceId = workspaceId,
+            UserRole = WorkspaceRole.Owner
+        };
+
+        using var uow = _unitOfWorkFactory.Create();
+
+        try
+        {
+            await uow.WorkspaceRepository.CreateAsync(
+                workspace, 
+                cancellationToken);
+
+            await uow.WorkspaceMemberRepository.AddMemberAsync(
+                workspaceMember, 
+                cancellationToken);
+
+            uow.Commit();
+        }
+        catch
+        {
+            uow.Rollback();
+            throw;
+        }
+
+        return workspaceId;
+    }
+}
