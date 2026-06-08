2db15f376ff3c8a269cc7a78ae2a5f471b177380
Tanya Tverdun <tanyatverdun@gmail.com>
2026-06-08 19:53:43 +0300

    feat: add domain entities and enums for workspaces and members

---
+++ WorkBoard.Domain/Entities/Workspace.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using WorkBoard.Domain.Common;
+using WorkBoard.Domain.Enums;
+
+namespace WorkBoard.Domain.Entities;
+
+[Table("Workspaces")]
+public class Workspace : BaseEntity<Guid>
+{
+    [Column("WorkspaceId")]
+    public override Guid Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public SubscriptionTier SubscriptionTier { get; set; } = 
+        SubscriptionTier.Free;
+
+    public DateTime CreatedAt { get; set; }
+    public Guid CreatedBy { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+    public Guid? UpdatedBy { get; set; }
+}
---
+++ WorkBoard.Domain/Entities/WorkspaceMember.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using WorkBoard.Domain.Enums;
+
+namespace WorkBoard.Domain.Entities;
+
+[Table("WorkspaceMembers")]
+public class WorkspaceMember
+{
+    public Guid UserId { get; set; }
+
+    public Guid WorkspaceId { get; set; }
+
+    public WorkspaceRole UserRole { get; set; } = 
+        WorkspaceRole.Member;
+}
