a38d8489af1f6874e7e8f2c638271dabfe731897
Tanya Tverdun <tanyatverdun@gmail.com>
2026-06-13 19:38:00 +0300

    feat: implement get boards by workspace id logic
    - Create BoardRepository
    - Add IsMemberAsync method to WorkspaceMemberRepository
    - Create BoardDto
    - Create GetBoardsByWorkspaceQuery and its handler

---
+++ WorkBoard.Application/Common/Dtos/Board/BoardDto.cs
@@ -0,0 +1,11 @@
+﻿using WorkBoard.Domain.Enums;
+
+namespace WorkBoard.Application.Common.Dtos.Board;
+
+public class BoardDto
+{
+    public Guid Id { get; set; }
+    public Guid WorkspaceId { get; set; }
+    public required string Name { get; set; }
+    public UserRole UserRole { get; set; }
+}
---
+++ WorkBoard.Application/Common/Interfaces/Repositories/IBoardRepository.cs
@@ -0,0 +1,12 @@
+﻿using WorkBoard.Application.Common.Dtos.Board;
+using WorkBoard.Domain.Entities;
+
+namespace WorkBoard.Application.Common.Interfaces.Repositories;
+
+public interface IBoardRepository : IGenericRepository<Board, Guid>
+{
+    Task<IReadOnlyList<BoardDto>> GetByWorkspaceIdAsync(
+        Guid workspaceId,
+        Guid userId,
+        CancellationToken cancellationToken = default);
+}
