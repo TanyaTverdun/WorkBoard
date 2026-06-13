de8c76dd87337d70ec2f3587ddbf8d090b811c1f
Tanya Tverdun <tanyatverdun@gmail.com>
2026-06-13 14:38:08 +0300

    "feat: add Update endpoint to WorkspacesController

--- WorkBoard.WebAPI/Controllers/WorkspacesController.cs
+++ WorkBoard.WebAPI/Controllers/WorkspacesController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WorkBoard.Application.Common.Dtos.Workspaces;
 using WorkBoard.Application.Features.Workspace.Commands.CreateWorkspace;
+using WorkBoard.Application.Features.Workspace.Commands.UpdateWorkspace;
 using WorkBoard.Application.Features.Workspace.Queries.GetUserWorkspaces;
 
 namespace WorkBoard.WebAPI.Controllers;
@@ -86,5 +87,54 @@ public class WorkspacesController : ControllerBase
         var workspaces = await _mediator.Send(query, cancellationToken);
 
         return Ok(workspaces);
+    }
+
+    /// <summary>
+    /// Updates an existing workspace details
+    /// </summary>
+    /// <param name="id">
+    /// The unique identifier of the workspace to update
+    /// </param>
+    /// <param name="command">
+    /// The updated workspace details (Name)
+    /// </param>
+    /// <param name="cancellationToken">
+    /// Cancellation token
+    /// </param>
+    /// <response code="204">
+    /// No Content. Success, workspace updated successfully
+    /// </response>
+    /// <response code="400">
+    /// Bad Request. If the input data is invalid
+    /// </response>
+    /// <response code="401">
+    /// Unauthorized. If the user is not authenticated
+    /// </response>
+    /// <response code="403">
+    /// Forbidden. If the user is not the owner of the workspace
+    /// </response>
+    /// <response code="404">
+    /// Not Found. If the workspace does not exist
+    /// </response>
+    /// <response code="500">
+    /// Internal Server Error. If a database error occurs
+    /// </response>
+    [HttpPut("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> Update(
+        [FromRoute] Guid id,
+        [FromBody] UpdateWorkspaceCommand command,
+        CancellationToken cancellationToken)
+    {
+        command.WorkspaceId = id;
+
+        await _mediator.Send(command, cancellationToken);
+
+        return NoContent();
     }
 }
