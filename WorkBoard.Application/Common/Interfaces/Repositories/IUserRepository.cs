9a492ac6b95cfa0a570ed03a4ccc30178fdaa5dd
Tanya Tverdun <tanyatverdun@gmail.com>
2026-06-13 13:27:31 +0300

    fix: duplicate user creation
    - Implemented `GetByIdOrEmailAsync` method in UserRepository

---
+++ WorkBoard.Application/Common/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using WorkBoard.Domain.Entities;
+
+namespace WorkBoard.Application.Common.Interfaces.Repositories;
+
+public interface IUserRepository : IGenericRepository<User, Guid>
+{
+     Task<User?> GetByIdOrEmailAsync(
+        Guid id,
+        string? email,
+        CancellationToken cancellationToken = default);
+}
