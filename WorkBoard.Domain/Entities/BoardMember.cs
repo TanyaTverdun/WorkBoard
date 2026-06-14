17bdd636425088a9b287d55b8f34f8c4bfb04e5d
Tanya Tverdun <tanyatverdun@gmail.com>
2026-06-14 13:41:54 +0300

    feat: add Board member entity and BoardRole enum

---
+++ WorkBoard.Domain/Entities/BoardMember.cs
@@ -0,0 +1,13 @@
+﻿using WorkBoard.Domain.Common;
+using WorkBoard.Domain.Enums;
+
+namespace WorkBoard.Domain.Entities;
+
+public class BoardMember : BaseEntity<(Guid, Guid)>
+{
+    public Guid UserId { get; set; }
+
+    public Guid BoardId { get; set; }
+
+    public BoardRole UserRole { get; set; }
+}
---
+++ WorkBoard.Domain/Enums/BoardRole.cs
@@ -0,0 +1,8 @@
+﻿namespace WorkBoard.Domain.Enums;
+
+public enum BoardRole : byte
+{
+    Member = 1,
+    Observer = 2
+}
+
