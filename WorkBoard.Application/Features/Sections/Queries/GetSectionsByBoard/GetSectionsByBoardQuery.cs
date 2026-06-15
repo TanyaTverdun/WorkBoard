2cf69337ead3a6a02da4dd8f6406823a2c4e7d7d
Tanya Tverdun <tanyatverdun@gmail.com>
2026-06-15 18:37:45 +0300

    feat: create get sesion bu boardId query and handler

---
+++ WorkBoard.Application/Features/Sections/Queries/GetSectionsByBoard/GetSectionsByBoardQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using WorkBoard.Application.Common.Dtos.Section;
+
+namespace WorkBoard.Application.Features.Sections.Queries.GetSectionsByBoard;
+
+public record GetSectionsByBoardQuery(
+    Guid BoardId) : IRequest<IReadOnlyList<SectionDto>>;
