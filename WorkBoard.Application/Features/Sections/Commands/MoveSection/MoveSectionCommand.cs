26db8e2e3448ad16c78dd3a79309557419e99cb3
Tanya Tverdun <tanyatverdun@gmail.com>
2026-06-16 12:47:59 +0300

    frea: create move section command, validator and handler

---
+++ WorkBoard.Application/Features/Sections/Commands/MoveSection/MoveSectionCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace WorkBoard.Application.Features.Sections.Commands.MoveSection;
+
+public record MoveSectionCommand(
+    Guid BoardId, 
+    Guid SectionId, 
+    double NewPosition) : IRequest<Unit>;
