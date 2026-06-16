55befded57b14d75ca2d29582bbe32f3311cee07
Tanya Tverdun <tanyatverdun@gmail.com>
2026-06-16 11:51:23 +0300

    feat: create CreateSection command, validator and handler

---
+++ WorkBoard.Application/Features/Sections/Commands/CreateSection/CreateSectionCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace WorkBoard.Application.Features.Sections.Commands.CreateSection;
+
+public class CreateSectionCommandValidator 
+    : AbstractValidator<CreateSectionCommand>
+{
+    public CreateSectionCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Section name is required.")
+            .MaximumLength(50)
+            .WithMessage("Section name must not exceed 50 characters.");
+    }
+}
