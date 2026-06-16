5ebe64c905d97902fb71121aad60d340cdf7e2b2
Tanya Tverdun <tanyatverdun@gmail.com>
2026-06-16 12:27:40 +0300

    feat: add UpdateSectionNameRequest and endpoint for section renaming

---
+++ WorkBoard.Application/Common/Dtos/Section/UpdateSectionNameRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace WorkBoard.Application.Common.Dtos.Section;
+
+public class UpdateSectionNameRequest
+{
+    public required string Name { get; set; }
+}
--- WorkBoard.WebAPI/Controllers/SectionsController.cs
+++ WorkBoard.WebAPI/Controllers/SectionsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WorkBoard.Application.Common.Dtos.Section;
 using WorkBoard.Application.Features.Sections.Commands.CreateSection;
+using WorkBoard.Application.Features.Sections.Commands.UpdateSectionName;
 using WorkBoard.Application.Features.Sections.Queries.GetSectionsByBoard;
 
 namespace WorkBoard.WebAPI.Controllers;
@@ -75,7 +76,7 @@ public class SectionsController : ControllerBase
     /// <returns>
     /// The unique identifier of the newly created section
     /// </returns>
-    /// <response code="201">
+    /// <response code="200">
     /// Returns the identifier of the created section
     /// </response>
     /// <response code="400">
@@ -91,7 +92,7 @@ public class SectionsController : ControllerBase
     /// If an internal server error occurs
     /// </response>
     [HttpPost]
-    [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(Guid))]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status403Forbidden)]
@@ -110,5 +111,63 @@ public class SectionsController : ControllerBase
             cancellationToken);
 
         return Ok(sectionId);
+    }
+
+    /// <summary>
+    /// Renames an existing section within a specific board
+    /// </summary>
+    /// <param name="boardId">
+    /// The unique identifier of the board
+    /// </param>
+    /// <param name="sectionId">
+    /// The unique identifier of the section to rename
+    /// </param>
+    /// <param name="request">
+    /// The request body containing the new name
+    /// </param>
+    /// <param name="cancellationToken">
+    /// Cancellation token provided by the runtime
+    /// </param>
+    /// <response code="204">
+    /// If the section was renamed successfully
+    /// </response>
+    /// <response code="400">
+    /// If the input data is invalid or name length exceeds limits
+    /// </response>
+    /// <response code="401">
+    /// If the user is not authenticated
+    /// </response>
+    /// <response code="403">
+    /// If the user does not have permission to modify this board
+    /// </response>
+    /// <response code="404">
+    /// If the section with the specified ID was not found on this board
+    /// </response>
+    /// <response code="500">
+    /// If an internal server error occurs
+    /// </response>
+    [HttpPut("{sectionId:guid}/name")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> Rename(
+        [FromRoute] Guid boardId,
+        [FromRoute] Guid sectionId,
+        [FromBody] UpdateSectionNameRequest request,
+        CancellationToken cancellationToken)
+    {
+        var command = new UpdateSectionNameCommand(
+            boardId, 
+            sectionId, 
+            request.Name);
+
+        await _mediator.Send(
+            command, 
+            cancellationToken);
+
+        return NoContent();
     }
 }
