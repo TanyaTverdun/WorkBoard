a03d2909a1214e8f218aa4b793b5e3ab261053e4
Tanya Tverdun <tanyatverdun@gmail.com>
2026-06-22 17:29:21 +0300

    feat: create GetBoardMembers query and handler

---
+++ WorkBoard.Application/Common/Dtos/BoardMembers/BoardMemberDto.cs
@@ -0,0 +1,9 @@
+﻿using WorkBoard.Domain.Enums;
+
+namespace WorkBoard.Application.Common.Dtos.BoardMembers;
+
+public record BoardMemberDto(
+    Guid UserId,
+    string FullName,
+    string Initials,
+    BoardRole UserRole);
---
+++ WorkBoard.Application/Features/Boards/Queries/GetBoardMembers/GetBoardMembersQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using WorkBoard.Application.Common.Dtos.BoardMembers;
+
+namespace WorkBoard.Application.Features.Boards.Queries.GetBoardMembers;
+
+public record GetBoardMembersQuery(
+    Guid BoardId) : IRequest<IReadOnlyList<BoardMemberDto>>;
