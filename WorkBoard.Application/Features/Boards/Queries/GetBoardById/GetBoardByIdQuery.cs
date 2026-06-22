5f7df3c23b5be5456984eae7a3644dd2ddd16cd6
Tanya Tverdun <tanyatverdun@gmail.com>
2026-06-22 11:40:48 +0300

    feat: create get board by id query and handler

---
+++ WorkBoard.Application/Features/Boards/Queries/GetBoardById/GetBoardByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using WorkBoard.Application.Common.Dtos.Board;
+
+namespace WorkBoard.Application.Features.Boards.Queries.GetBoardById;
+
+public record GetBoardByIdQuery(
+    Guid BoardId) : IRequest<BoardDto>;
---
+++ WorkBoard.Application/Features/Boards/Queries/GetBoardById/GetBoardByIdQueryHandler.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using MediatR;
+using WorkBoard.Application.Common.Dtos.Board;
+using WorkBoard.Application.Common.Exceptions;
+using WorkBoard.Application.Common.Interfaces;
+using WorkBoard.Application.Common.Interfaces.Repositories;
+
+namespace WorkBoard.Application.Features.Boards.Queries.GetBoardById;
+
+public class GetBoardByIdQueryHandler 
+    : IRequestHandler<GetBoardByIdQuery, BoardDto>
+{
+    private readonly IBoardRepository _boardRepository;
+    private readonly IBoardMemberRepository _memberRepository;
+    private readonly IUserContext _userContext;
+    private readonly IMapper _mapper;
+
+    public GetBoardByIdQueryHandler(
+        IBoardRepository boardRepository,
+        IBoardMemberRepository memberRepository,
+        IUserContext userContext,
+        IMapper mapper)
+    {
+        _boardRepository = boardRepository;
+        _memberRepository = memberRepository;
+        _userContext = userContext;
+        _mapper = mapper;
+    }
+
+    public async Task<BoardDto> Handle(
+        GetBoardByIdQuery query,
+        CancellationToken cancellationToken)
+    {
+        var userId = _userContext.UserId;
+
+        if (userId == null)
+        {
+            throw new UnauthorizedAccessException(
+                 "User is not authenticated.");
+        }
+
+        var isMember = await _memberRepository.IsMemberAsync(
+            query.BoardId,
+            userId.Value,
+            cancellationToken);
+
+        if (!isMember)
+        {
+            throw new ForbiddenAccessException(
+                "You do not have access to this board.");
+        }
+
+        var board = await _boardRepository.GetByIdAsync(
+            query.BoardId, 
+            cancellationToken);
+
+        if (board == null)
+        {
+            throw new NotFoundException(
+                $"Board with ID {query.BoardId} was not found.");
+        }
+
+        return _mapper.Map<BoardDto>(board);
+    }
+}
