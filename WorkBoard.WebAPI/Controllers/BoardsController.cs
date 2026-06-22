0a12f0ad34cc719ec871dc12901f1a1fd7be461b
Tanya Tverdun <tanyatverdun@gmail.com>
2026-06-22 21:22:11 +0300

    feat: add RemoveBoardMember endpoint to BoardsController

--- WorkBoard.WebAPI/Controllers/BoardsController.cs
+++ WorkBoard.WebAPI/Controllers/BoardsController.cs
@@ -6,6 +6,7 @@ using WorkBoard.Application.Common.Dtos.BoardMembers;
 using WorkBoard.Application.Features.Boards.Commands.AddBoardMember;
 using WorkBoard.Application.Features.Boards.Commands.CreateBoard;
 using WorkBoard.Application.Features.Boards.Commands.DeleteBoard;
+using WorkBoard.Application.Features.Boards.Commands.RemoveBoardMember;
 using WorkBoard.Application.Features.Boards.Commands.UpdateBoard;
 using WorkBoard.Application.Features.Boards.Commands.UpdateMemberRole;
 using WorkBoard.Application.Features.Boards.Queries.GetBoardById;
@@ -371,6 +372,58 @@ public class BoardsController : ControllerBase
             boardId,
             request.UserId,
             request.Role);
+
+        await _mediator.Send(
+            command, 
+            cancellationToken);
+
+        return Ok();
+    }
+
+    /// <summary>
+    /// Removes a member from the specified board
+    /// </summary>
+    /// <param name="boardId">
+    /// The unique identifier of the board
+    /// </param>
+    /// <param name="userId">
+    /// The unique identifier of the user to remove
+    /// </param>
+    /// <param name="cancellationToken">
+    /// The cancellation token
+    /// </param>
+    /// <returns>
+    /// An empty OK response if the member was successfully removed
+    /// </returns>
+    /// <response code="200">
+    /// The member was successfully removed from the board
+    /// </response>
+    /// <response code="401">
+    /// The user is not authenticated
+    /// </response>
+    /// <response code="403">
+    /// The user does not have permission to remove members
+    /// </response>
+    /// <response code="404">
+    /// The board or the member was not found
+    /// </response>
+    /// <response code="500">
+    /// An internal server error occurred
+    /// </response>
+    [HttpDelete("{boardId:guid}/members/{userId:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> RemoveBoardMember(
+        Guid boardId,
+        Guid userId,
+        CancellationToken cancellationToken)
+    {
+        var command = new RemoveBoardMemberCommand(
+            boardId, 
+            userId);
 
         await _mediator.Send(
             command, 
