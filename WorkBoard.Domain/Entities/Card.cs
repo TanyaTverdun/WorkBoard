300971e0270a77c68873884686be7fd9c7cbf566
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-06-24 13:08:57 +0300

    feat: add Card entity

---
+++ WorkBoard.Domain/Entities/Card.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using WorkBoard.Domain.Common;
+
+namespace WorkBoard.Domain.Entities;
+
+[Table("Cards")]
+public class Card : BaseEntity<Guid>
+{
+    [Column("CardId")]
+    public override Guid Id { get; set; }
+
+    public required Guid SectionId { get; set; }
+
+    public required string Title { get; set; }
+
+    public string? Description { get; set; }
+
+    public DateTime? DueDate { get; set; }
+
+    public required double Position { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public Guid CreatedBy { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public Guid? UpdatedBy { get; set; }
+}
