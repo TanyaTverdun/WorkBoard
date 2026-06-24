17778b5881fe0dfd4a07c4e2d5ab4ca7d4507efc
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-06-24 17:51:18 +0300

    feat: create GetCardsByBoard query and handler

---
+++ WorkBoard.Application/Features/Cards/Queries/GetCardsByBoard/GetCardsByBoardQueryHandler.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using MediatR;
+using WorkBoard.Application.Common.Dtos.Cards;
+using WorkBoard.Application.Common.Exceptions;
+using WorkBoard.Application.Common.Interfaces;
+using WorkBoard.Application.Common.Interfaces.Repositories;
+
+namespace WorkBoard.Application.Features.Cards.Queries.GetCardsByBoard;
+
+public class GetCardsByBoardQueryHandler
+: IRequestHandler<GetCardsByBoardQuery, IReadOnlyList<CardDto>>
+{
+    private readonly ICardRepository _cardRepository;
+    private readonly IMapper _mapper;
+    private readonly IBoardMemberRepository _boardMemberRepository;
+    private readonly IUserContext _userContext;
+
+    public GetCardsByBoardQueryHandler(
+        ICardRepository cardRepository,
+        IMapper mapper,
+        IBoardMemberRepository boardMemberRepository,
+        IUserContext userContext)
+    {
+        _cardRepository = cardRepository;
+        _mapper = mapper;
+        _boardMemberRepository = boardMemberRepository;
+        _userContext = userContext;
+    }
+
+    public async Task<IReadOnlyList<CardDto>> Handle(
+        GetCardsByBoardQuery request,
+        CancellationToken cancellationToken)
+    {
+        var userId = _userContext.UserId;
+
+        if (userId == null)
+        {
+            throw new UnauthorizedAccessException(
+                "User is not authenticated.");
+        }
+
+        var isBoardMember = await _boardMemberRepository.IsMemberAsync(
+            request.BoardId,
+            userId.Value,
+            cancellationToken);
+
+        if (!isBoardMember)
+        {
+            throw new ForbiddenAccessException(
+                "You do not have access to this board.");
+        }
+
+        var cards = await _cardRepository.GetCardsByBoardIdAsync(
+            request.BoardId,
+            cancellationToken);
+
+        return _mapper.Map<IReadOnlyList<CardDto>>(cards);
+    }
+}
