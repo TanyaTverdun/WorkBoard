2448390c019ec09ca980bb1a30896d7f3867fcad
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-06-30 14:48:34 +0300

    feat: add UpdateCardTitle command, validator and handler

---
+++ WorkBoard.Application/Features/Cards/Commands/UpdateCardTitle/UpdateCardTitleCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace WorkBoard.Application.Features.Cards.Commands.UpdateCardTitle;
+
+public class UpdateCardTitleCommandValidator 
+    : AbstractValidator<UpdateCardTitleCommand>
+{
+    public UpdateCardTitleCommandValidator()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty()
+            .WithMessage("Card title is required.")
+            .MaximumLength(100)
+            .WithMessage("Card title must not exceed 100 characters.");
+    }
+}
