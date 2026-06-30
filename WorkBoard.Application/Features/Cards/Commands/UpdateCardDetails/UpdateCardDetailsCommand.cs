5ddd1c3723b5eb512df1d32ad51268a1bab963f9
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-06-30 13:53:03 +0300

    feat: add UpdateCardDetails command, validator and handler

---
+++ WorkBoard.Application/Features/Cards/Commands/UpdateCardDetails/UpdateCardDetailsCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace WorkBoard.Application.Features.Cards.Commands.UpdateCardDetails;
+
+public record UpdateCardDetailsCommand(
+    Guid BoardId,
+    Guid CardId,
+    string? Title,
+    string? Description,
+    bool IsDescriptionUpdated) : IRequest<Unit>;
