674d36be822e1b333eb0fe8b260f7da2b87dd31e
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-06-30 21:22:38 +0300

    feat: add CardLabel and Lable entities

---
+++ WorkBoard.Domain/Entities/CardLabel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using WorkBoard.Domain.Common;
+
+namespace WorkBoard.Domain.Entities;
+
+[Table("CardLabels")]
+public class CardLabel : BaseEntity<(Guid, Guid)>
+{
+    public Guid CardId { get; set; }
+
+    public Guid LabelId { get; set; }
+}
---
+++ WorkBoard.Domain/Entities/Label.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using WorkBoard.Domain.Common;
+
+namespace WorkBoard.Domain.Entities;
+
+[Table("Labels")]
+public class Label : BaseEntity<Guid>
+{
+    [Column("LabelId")]
+    public override Guid Id { get; set; }
+
+    public Guid BoardId { get; set; }
+
+    public required string Name { get; set; }
+
+    public string? Color { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+    public Guid CreatedBy { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+    public Guid? UpdatedBy { get; set; }
+}
