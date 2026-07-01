1b02062e56b517a65c7e512ac89152ae1d3a45e2
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-01 17:12:56 +0300

    feat: add GetCardAssignees endpoint to CardsController

--- WorkBoard.WebAPI/Controllers/CardsController.cs
+++ WorkBoard.WebAPI/Controllers/CardsController.cs
@@ -7,6 +7,7 @@ using WorkBoard.Application.Features.Cards.Commands.DeleteCard;
 using WorkBoard.Application.Features.Cards.Commands.MoveCard;
 using WorkBoard.Application.Features.Cards.Commands.UpdateCardDescription;
 using WorkBoard.Application.Features.Cards.Commands.UpdateCardTitle;
+using WorkBoard.Application.Features.Cards.Queries.GetCardAssignees;
 using WorkBoard.Application.Features.Cards.Queries.GetCardsByBoard;
 
 namespace WorkBoard.WebAPI.Controllers;
@@ -321,5 +322,52 @@ public class CardsController : ControllerBase
         await _mediator.Send(command, cancellationToken);
 
         return NoContent();
+    }
+
+    /// <summary>
+    /// Retrieves a list of all users assigned to a specific card
+    /// </summary>
+    /// <param name="cardId">
+    /// The unique identifier of the card
+    /// </param>
+    /// <param name="cancellationToken">
+    /// Cancellation token provided by the runtime
+    /// </param>
+    /// <returns>
+    /// A list of assigned users with their basic information and initials
+    /// </returns>
+    /// <response code="200">
+    /// Returns the list of card assignees successfully
+    /// </response>
+    /// <response code="401">
+    /// If the user is not authenticated
+    /// </response>
+    /// <response code="403">
+    /// If the user does not have permission to access this board
+    /// </response>
+    /// <response code="404">
+    /// If the card with the specified ID or its section was not found
+    /// </response>
+    /// <response code="500">
+    /// If an internal server error occurs while processing the request
+    /// </response>
+    [HttpGet("/api/cards/{cardId:guid}/assignees")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetCardAssignees(
+        [FromRoute] Guid cardId,
+        CancellationToken cancellationToken)
+    {
+        var query = new GetCardAssigneesQuery 
+        { 
+            CardId = cardId 
+        };
+
+        var result = await _mediator.Send(query, cancellationToken);
+
+        return Ok(result);
     }
 }
