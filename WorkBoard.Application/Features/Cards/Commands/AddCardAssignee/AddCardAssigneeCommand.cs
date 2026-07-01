3932433842708dfc1ab7d2efa133bef4c8643dfe
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-01 17:27:58 +0300

    feat: add AddCardAssignee command and handler

---
+++ WorkBoard.Application/Features/Cards/Commands/AddCardAssignee/AddCardAssigneeCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace WorkBoard.Application.Features.Cards.Commands.AddCardAssignee;
+
+public record AddCardAssigneeCommand : IRequest<Unit>
+{
+    public Guid CardId { get; init; }
+    public Guid TargetUserId { get; init; }
+}
