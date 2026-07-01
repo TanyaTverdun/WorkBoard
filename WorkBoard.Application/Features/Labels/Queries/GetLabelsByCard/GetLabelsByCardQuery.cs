5aecdcc30bf4243db19df49dc13aa7465fc32e09
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-01 10:31:28 +0300

    feat: add GetLabelsByCard query and handler

---
+++ WorkBoard.Application/Features/Labels/Queries/GetLabelsByCard/GetLabelsByCardQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using WorkBoard.Application.Common.Dtos.Labels;
+
+namespace WorkBoard.Application.Features.Labels.Queries.GetLabelsByCard;
+
+public class GetLabelsByCardQuery : IRequest<IReadOnlyList<LabelDto>>
+{
+    public Guid CardId { get; set; }
+}
