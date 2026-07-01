80a961e919149088e8241cf1769f1c5b3df9c1bd
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-01 10:35:57 +0300

    feat: add GetLabelsByCard endpoint to LabelsController

--- WorkBoard.WebAPI/Controllers/LabelsController.cs
+++ WorkBoard.WebAPI/Controllers/LabelsController.cs
@@ -2,8 +2,13 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WorkBoard.Application.Common.Dtos.Labels;
+using WorkBoard.Application.Features.Labels.Commands.AddLabelToCard;
 using WorkBoard.Application.Features.Labels.Commands.CreateLabel;
+using WorkBoard.Application.Features.Labels.Commands.DeleteLabel;
+using WorkBoard.Application.Features.Labels.Commands.RemoveLabelFromCard;
+using WorkBoard.Application.Features.Labels.Commands.UpdateLabel;
 using WorkBoard.Application.Features.Labels.Queries.GetLabelsByBoard;
+using WorkBoard.Application.Features.Labels.Queries.GetLabelsByCard;
 
 namespace WorkBoard.WebAPI.Controllers;
 
@@ -123,4 +128,54 @@ public class LabelsController : ControllerBase
 
         return Ok(result);
     }
+
+    /// <summary>
+    /// Gets all labels attached to a specific card
+    /// </summary>
+    /// <param name="cardId">
+    /// The unique identifier of the card
+    /// </param>
+    /// <param name="cancellationToken">
+    /// Cancellation token provided by the runtime
+    /// </param>
+    /// <returns>
+    /// A list of labels attached to the card
+    /// </returns>
+    /// <response code="200">
+    /// Returns the list of attached labels successfully
+    /// </response>
+    /// <response code="401">
+    /// If the user is not authenticated
+    /// </response>
+    /// <response code="403">
+    /// If the user does not have permission to access this card
+    /// </response>
+    /// <response code="404">
+    /// If the card with the specified ID was not found
+    /// </response>
+    /// <response code="500">
+    /// If an internal server error occurs while processing the request
+    /// </response>
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IReadOnlyList<LabelDto>))]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetLabelsByCard(
+        [FromRoute] Guid cardId,
+        CancellationToken cancellationToken)
+    {
+        var query = new GetLabelsByCardQuery
+        {
+            CardId = cardId
+        };
+
+        var result = await _mediator.Send(
+            query,
+            cancellationToken);
+
+        return Ok(result);
+    }
+
 }
