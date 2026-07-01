ccae65b35e6bb07fba7f96806d414be4d164851c
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-01 14:22:38 +0300

    feat: add DeleteCard command and handler

---
+++ WorkBoard.Application/Features/Cards/Commands/DeleteCard/DeleteCardCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace WorkBoard.Application.Features.Cards.Commands.DeleteCard;
+
+public class DeleteCardCommand : IRequest<Unit>
+{
+    public Guid BoardId { get; set; }
+    public Guid CardId { get; set; }
+}
---
+++ WorkBoard.Application/Features/Cards/Commands/DeleteCard/DeleteCardCommandHandler.cs
@@ -0,0 +1,62 @@
+﻿using MediatR;
+using WorkBoard.Application.Common.Exceptions;
+using WorkBoard.Application.Common.Interfaces;
+using WorkBoard.Domain.Enums;
+
+namespace WorkBoard.Application.Features.Cards.Commands.DeleteCard;
+
+public class DeleteCardCommandHandler 
+    : IRequestHandler<DeleteCardCommand, Unit>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+    private readonly IUserContext _userContext;
+
+    public DeleteCardCommandHandler(
+        IUnitOfWorkFactory unitOfWorkFactory,
+        IUserContext userContext)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+        _userContext = userContext;
+    }
+
+    public async Task<Unit> Handle(
+        DeleteCardCommand request,
+        CancellationToken cancellationToken)
+    {
+        var currentUserId = _userContext.UserId
+            ?? throw new UnauthorizedAccessException(
+                "User is not authenticated.");
+
+        using var uow = _unitOfWorkFactory.Create();
+
+        var membership = await uow.BoardMemberRepository.GetMembershipAsync(
+            currentUserId,
+            request.BoardId,
+            cancellationToken);
+
+        if (membership == null || membership.UserRole == BoardRole.Observer)
+        {
+            throw new ForbiddenAccessException(
+                "You do not have permission to delete cards on this board.");
+        }
+
+        var card = await uow.CardRepository.GetByIdAsync(
+            request.CardId,
+            cancellationToken)
+                ?? throw new NotFoundException(
+                    $"Card with ID {request.CardId} was not found.");
+
+        try
+        {
+            await uow.CardRepository.DeleteAsync(card.Id, cancellationToken);
+            uow.Commit();
+        }
+        catch
+        {
+            uow.Rollback();
+            throw;
+        }
+
+        return Unit.Value;
+    }
+}
