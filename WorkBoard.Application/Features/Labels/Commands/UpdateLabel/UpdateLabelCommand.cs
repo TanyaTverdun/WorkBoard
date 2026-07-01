bad1bc827dabbb7ad5167d5bd4fbaa898cdbbb72
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-01 10:43:43 +0300

    feat: add UpdateLabel command, validator and handler

---
+++ WorkBoard.Application/Features/Labels/Commands/UpdateLabel/UpdateLabelCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using WorkBoard.Application.Common.Dtos.Labels;
+
+namespace WorkBoard.Application.Features.Labels.Commands.UpdateLabel;
+
+public class UpdateLabelCommand : IRequest<LabelDto>
+{
+    public Guid LabelId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Color { get; set; } = string.Empty;
+}
---
+++ WorkBoard.Application/Features/Labels/Commands/UpdateLabel/UpdateLabelCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace WorkBoard.Application.Features.Labels.Commands.UpdateLabel;
+
+public class UpdateLabelCommandValidator : AbstractValidator<UpdateLabelCommand>
+{
+    public UpdateLabelCommandValidator()
+    {
+        RuleFor(v => v.Name)
+            .NotEmpty()
+            .WithMessage("Name is required.")
+            .MaximumLength(50)
+            .WithMessage("Name must not exceed 50 characters.");
+
+        RuleFor(v => v.Color)
+            .MaximumLength(9)
+            .WithMessage("Color must not exceed 9 characters.")
+            .Matches("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{8})$")
+            .When(v => !string.IsNullOrEmpty(v.Color))
+            .WithMessage("Color must be a valid HEX format (#172B4D or #172B4D80).");
+    }
+}
