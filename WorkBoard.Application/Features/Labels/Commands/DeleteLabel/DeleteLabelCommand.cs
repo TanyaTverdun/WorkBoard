0637586d9739dac116f3ec8a65149178c02bab92
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-01 10:46:59 +0300

    feat: add DeleteLabel command and handler

---
+++ WorkBoard.Application/Features/Labels/Commands/DeleteLabel/DeleteLabelCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace WorkBoard.Application.Features.Labels.Commands.DeleteLabel;
+
+public class DeleteLabelCommand : IRequest<Unit>
+{
+    public Guid LabelId { get; set; }
+}
