2576dcb22a08e869b885fa5dcc5ae42102d92d47
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-02 15:48:48 +0300

    feat: add GetChecklistsByCard and CreateChecklist endpoints to ChecklistsController

---
+++ WorkBoard.WebAPI/Controllers/ChecklistsController.cs
@@ -0,0 +1,121 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WorkBoard.Application.Common.Checklists;
+using WorkBoard.Application.Features.Checklists.Commands.CreateChecklist;
+using WorkBoard.Application.Features.Checklists.Queries.GetChecklistsByCard;
+
+namespace WorkBoard.WebAPI.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/cards/{cardId:guid}/checklists")]
+public class ChecklistsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ChecklistsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    /// <summary>
+    /// Gets all checklists attached to a specific card
+    /// </summary>
+    /// <param name="cardId">
+    /// The unique identifier of the card
+    /// </param>
+    /// <param name="cancellationToken">
+    /// Cancellation token provided by the runtime
+    /// </param>
+    /// <returns>
+    /// A list of checklists belonging to the card
+    /// </returns>
+    /// <response code="200">
+    /// Returns the list of checklists successfully
+    /// </response>
+    /// <response code="401">
+    /// If the user is not authenticated
+    /// </response>
+    /// <response code="403">
+    /// If the user does not have permission to access this card
+    /// </response>
+    /// <response code="404">
+    /// If the card with the specified ID was not found
+    /// </response>
+    /// <response code="500">
+    /// If an internal server error occurs while processing the request
+    /// </response>
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetChecklistsByCard(
+        [FromRoute] Guid cardId,
+        CancellationToken cancellationToken)
+    {
+        var query = new GetChecklistsByCardQuery(cardId);
+
+        var result = await _mediator.Send(query, cancellationToken);
+
+        return Ok(result);
+    }
+
+    /// <summary>
+    /// Creates a new checklist for a specific card
+    /// </summary>
+    /// <param name="cardId">
+    /// The unique identifier of the card to which the checklist will belong
+    /// </param>
+    /// <param name="request">
+    /// The checklist details (Name)
+    /// </param>
+    /// <param name="cancellationToken">
+    /// Cancellation token provided by the runtime
+    /// </param>
+    /// <returns>
+    /// The details of the newly created checklist
+    /// </returns>
+    /// <response code="200">
+    /// Returns the created checklist successfully
+    /// </response>
+    /// <response code="400">
+    /// If the input data is invalid
+    /// </response>
+    /// <response code="401">
+    /// If the user is not authenticated
+    /// </response>
+    /// <response code="403">
+    /// If the user does not have permission to modify this card
+    /// </response>
+    /// <response code="404">
+    /// If the card with the specified ID was not found
+    /// </response>
+    /// <response code="500">
+    /// If an internal server error occurs while processing the request
+    /// </response>
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ChecklistDto))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> CreateChecklist(
+        [FromRoute] Guid cardId,
+        [FromBody] CreateChecklistRequest request,
+        CancellationToken cancellationToken)
+    {
+        var command = new CreateChecklistCommand
+        {
+            CardId = cardId,
+            Name = request.Name
+        };
+
+        var result = await _mediator.Send(command, cancellationToken);
+
+        return Ok(result);
+    }
+}
