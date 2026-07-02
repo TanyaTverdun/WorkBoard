2cd58140c1dcd4a3a8c683807b5ffee827b2fda2
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-02 15:47:33 +0300

    feat: create GetChecklistsByCard query and handler

---
+++ WorkBoard.Application/Features/Checklists/Queries/GetChecklistsByCard/GetChecklistsByCardQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using WorkBoard.Application.Common.Checklists;
+
+namespace WorkBoard.Application.Features.Checklists.Queries.GetChecklistsByCard;
+
+public record GetChecklistsByCardQuery(Guid CardId) 
+    : IRequest<IReadOnlyList<ChecklistItemDto>>;
---
+++ WorkBoard.Application/Features/Checklists/Queries/GetChecklistsByCard/GetChecklistsByCardQueryHandler.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using MediatR;
+using WorkBoard.Application.Common.Checklists;
+using WorkBoard.Application.Common.Exceptions;
+using WorkBoard.Application.Common.Interfaces;
+using WorkBoard.Application.Common.Interfaces.Repositories;
+
+namespace WorkBoard.Application.Features.Checklists.Queries.GetChecklistsByCard;
+
+public class GetChecklistsByCardQueryHandler
+: IRequestHandler<GetChecklistsByCardQuery, IReadOnlyList<ChecklistItemDto>>
+{
+    private readonly IChecklistRepository _checklistRepository;
+    private readonly ICardRepository _cardRepository;
+    private readonly ISectionRepository _sectionRepository;
+    private readonly IBoardMemberRepository _boardMemberRepository;
+    private readonly IMapper _mapper;
+    private readonly IUserContext _userContext;
+
+    public GetChecklistsByCardQueryHandler(
+        IChecklistRepository checklistRepository,
+        ICardRepository cardRepository,
+        ISectionRepository sectionRepository,
+        IBoardMemberRepository boardMemberRepository,
+        IMapper mapper,
+        IUserContext userContext)
+    {
+        _checklistRepository = checklistRepository;
+        _cardRepository = cardRepository;
+        _sectionRepository = sectionRepository;
+        _boardMemberRepository = boardMemberRepository;
+        _mapper = mapper;
+        _userContext = userContext;
+    }
+
+    public async Task<IReadOnlyList<ChecklistItemDto>> Handle(
+        GetChecklistsByCardQuery request,
+        CancellationToken cancellationToken)
+    {
+        var userId = _userContext.UserId
+            ?? throw new UnauthorizedAccessException(
+                "User is not authenticated.");
+
+        var card = await _cardRepository.GetByIdAsync(
+            request.CardId,
+            cancellationToken)
+                ?? throw new NotFoundException(
+                    $"Card with ID {request.CardId} was not found.");
+
+        var section = await _sectionRepository.GetByIdAsync(
+            card.SectionId,
+            cancellationToken)
+                ?? throw new NotFoundException(
+                    $"Section with ID {card.SectionId} was not found.");
+
+        var isBoardMember = await _boardMemberRepository.IsMemberAsync(
+            section.BoardId,
+            userId,
+            cancellationToken);
+
+        if (!isBoardMember)
+        {
+            throw new ForbiddenAccessException(
+                "You do not have access to this board's checklists.");
+        }
+
+        var checklists = await _checklistRepository.GetByCardIdAsync(
+            request.CardId,
+            cancellationToken);
+
+        return _mapper.Map<IReadOnlyList<ChecklistItemDto>>(checklists);
+    }
+}
