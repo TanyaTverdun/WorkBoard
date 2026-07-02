8126924874cf5d0ec432db72f51f2ea0b5dded7b
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-02 16:00:28 +0300

    feat: create UpdateChecklist command, validator and handler

---
+++ WorkBoard.Application/Features/Checklists/Commands/UpdateChecklist/UpdateChecklistCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace WorkBoard.Application.Features.Checklists.Commands.UpdateChecklist;
+
+public class UpdateChecklistCommandValidator 
+    : AbstractValidator<UpdateChecklistCommand>
+{
+    public UpdateChecklistCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Checklist name is required.")
+            .MaximumLength(50)
+            .WithMessage("Checklist name must not exceed 50 characters.");
+    }
+}
