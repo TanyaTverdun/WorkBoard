2cef9b6599bfa56cea32f3655bf8b52082115c85
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-02 20:16:22 +0300

    feat: add DeleteChecklist command and handle

---
+++ WorkBoard.Application/Features/Checklists/Commands/DeleteChecklist/DeleteChecklistCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace WorkBoard.Application.Features.Checklists.Commands.DeleteChecklist;
+
+public class DeleteChecklistCommand : IRequest
+{
+    public Guid ChecklistId { get; set; }
+}
---
+++ WorkBoard.Application/Features/Checklists/Commands/DeleteChecklist/DeleteChecklistCommandHandler.cs
@@ -0,0 +1,73 @@
+﻿using MediatR;
+using WorkBoard.Application.Common.Exceptions;
+using WorkBoard.Application.Common.Interfaces;
+
+namespace WorkBoard.Application.Features.Checklists.Commands.DeleteChecklist;
+
+public class DeleteChecklistCommandHandler : IRequestHandler<DeleteChecklistCommand>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+    private readonly IUserContext _userContext;
+
+    public DeleteChecklistCommandHandler(
+        IUnitOfWorkFactory unitOfWorkFactory,
+        IUserContext userContext)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+        _userContext = userContext;
+    }
+
+    public async Task Handle(
+        DeleteChecklistCommand request,
+        CancellationToken cancellationToken)
+    {
+        var currentUserId = _userContext.UserId
+            ?? throw new UnauthorizedAccessException(
+                "User is not authenticated.");
+
+        using var uow = _unitOfWorkFactory.Create();
+
+        var checklist = await uow.ChecklistRepository.GetByIdAsync(
+            request.ChecklistId, 
+            cancellationToken)
+                ?? throw new NotFoundException(
+                    $"Checklist with ID {request.ChecklistId} was not found.");
+
+        var card = await uow.CardRepository.GetByIdAsync(
+            checklist.CardId, 
+            cancellationToken)
+                ?? throw new NotFoundException(
+                    $"Card with ID {checklist.CardId} was not found.");
+
+        var section = await uow.SectionRepository.GetByIdAsync(
+            card.SectionId, 
+            cancellationToken)
+                ?? throw new NotFoundException(
+                    $"Section with ID {card.SectionId} was not found.");
+
+        var isCurrentMember = await uow.BoardMemberRepository.IsMemberAsync(
+            section.BoardId,
+            currentUserId,
+            cancellationToken);
+
+        if (!isCurrentMember)
+        {
+            throw new ForbiddenAccessException(
+                "You do not have access to modify this checklist.");
+        }
+
+        try
+        {
+            await uow.ChecklistRepository.DeleteAsync(
+                checklist.Id, 
+                cancellationToken);
+
+            uow.Commit();
+        }
+        catch
+        {
+            uow.Rollback();
+            throw;
+        }
+    }
+}
