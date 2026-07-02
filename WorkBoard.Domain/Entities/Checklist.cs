b03f1b59d49a9ffa18fe9fa115d18f536e036a86
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-02 15:45:56 +0300

    feat: create Checklist entity

---
+++ WorkBoard.Domain/Entities/Checklist.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using WorkBoard.Domain.Common;
+
+namespace WorkBoard.Domain.Entities;
+
+[Table("Checklists")]
+public class Checklist : BaseEntity<Guid>
+{
+    [Column("ChecklistId")]
+    public Guid Id { get; set; }
+    public Guid CardId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+    public Guid CreatedBy { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public Guid? UpdatedBy { get; set; }
+}
