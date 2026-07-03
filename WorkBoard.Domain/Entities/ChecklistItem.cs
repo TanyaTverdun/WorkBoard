8016b7ad1259969df3867415e36c5756ec6d2173
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-03 11:47:54 +0300

    feat: add ChecklistItem entity

---
+++ WorkBoard.Domain/Entities/ChecklistItem.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using WorkBoard.Domain.Common;
+
+namespace WorkBoard.Domain.Entities;
+
+[Table("Checklist_items")]
+public class ChecklistItem : BaseEntity<Guid>
+{
+    [Column("ChecklistItemId")]
+    public override Guid Id { get; set; }
+
+    public Guid ChecklistId { get; set; }
+
+    public required string Title { get; set; }
+
+    public bool IsDone { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+    public Guid CreatedBy { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+    public Guid? UpdatedBy { get; set; }
+}
