95e8437df511718f7145c565f358cac7b47aef50
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-07 16:04:38 +0300

    feat: add DuplicateTitleException

---
+++ WorkBoard.Application/Common/Exceptions/DuplicateTitleException.cs
@@ -0,0 +1,14 @@
+﻿namespace WorkBoard.Application.Common.Exceptions;
+
+public class DuplicateTitleException : Exception
+{
+    public DuplicateTitleException()
+        : base("An item with this title already exists.")
+    {
+    }
+
+    public DuplicateTitleException(string message)
+        : base(message)
+    {
+    }
+}
