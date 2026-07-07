db9527f28a2d08547eda374b5df776a7d419a5a5
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-07 16:05:42 +0300

    feat: add AddChecklistItem command, validator and handler

---
+++ WorkBoard.Application/Features/Checklists/Commands/AddChecklistItem/AddChecklistItemCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using WorkBoard.Application.Common.Dtos.Checklists;
+
+namespace WorkBoard.Application.Features.Checklists.Commands.AddChecklistItem;
+
+public class AddChecklistItemCommand : IRequest<ChecklistItemDto>
+{
+    public Guid ChecklistId { get; set; }
+    public required string Title { get; set; }
+}
