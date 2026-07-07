826cfb7bf8a30c2f2483917eeaebe49721002759
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-07 16:40:33 +0300

    feat: add UpdateChecklistItemStatus command and handler

---
+++ WorkBoard.Application/Features/Checklists/Commands/UpdateChecklistItemStatus/UpdateChecklistItemStatusCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using WorkBoard.Application.Common.Dtos.Checklists;
+
+namespace WorkBoard.Application.Features.Checklists.Commands.UpdateChecklistItemStatus;
+
+public class UpdateChecklistItemStatusCommand : IRequest<ChecklistItemDto>
+{
+    public Guid ItemId { get; set; }
+    public bool IsDone { get; set; }
+}
