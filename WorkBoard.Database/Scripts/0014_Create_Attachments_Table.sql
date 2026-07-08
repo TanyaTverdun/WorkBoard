479b0d388dd23d7462c3680bc8c45da9f29f9dbc
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-09 00:39:19 +0300

    feat: add CreatedAt and CreatedBy fields to Attachments table

--- WorkBoard.Database/Scripts/0014_Create_Attachments_Table.sql
+++ WorkBoard.Database/Scripts/0014_Create_Attachments_Table.sql
@@ -4,9 +4,15 @@ CREATE TABLE [Attachments] (
     [FileUrl]       NVARCHAR(2048)   NOT NULL,
     [FileName]      NVARCHAR(100)    NOT NULL,
     [FileSizeBytes] BIGINT           NOT NULL,
+    [CreatedAt]     DATETIME2        NOT NULL,
+    [CreatedBy]     UNIQUEIDENTIFIER NOT NULL,
 
     CONSTRAINT [FK_Attachments_Cards] 
         FOREIGN KEY ([CardId])
         REFERENCES [Cards]([CardId])
-        ON DELETE CASCADE
+        ON DELETE CASCADE,
+
+    CONSTRAINT [FK_Attachments_CreatedBy_Users] 
+        FOREIGN KEY ([CreatedBy]) 
+        REFERENCES [Users]([UserId])
 );
