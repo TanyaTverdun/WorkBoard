c42766f6a590e5249b18b80e648bae4b755fd0c1
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-08 23:30:51 +0300

    feat: add AddAttachment command, validator and handler

---
+++ WorkBoard.Application/Features/Attachments/Commands/AddAttachment/AddAttachmentCommandHandler.cs
@@ -0,0 +1,92 @@
+﻿using AutoMapper;
+using MediatR;
+using WorkBoard.Application.Common.Dtos.Attachments;
+using WorkBoard.Application.Common.Exceptions;
+using WorkBoard.Application.Common.Interfaces;
+using WorkBoard.Application.Common.Interfaces.BlobStorage;
+using WorkBoard.Domain.Entities;
+
+namespace WorkBoard.Application.Features.Attachments.Commands.AddAttachment;
+
+public class AddAttachmentCommandHandler
+    : IRequestHandler<AddAttachmentCommand, AttachmentDto>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+    private readonly IUserContext _userContext;
+    private readonly IMapper _mapper;
+    private readonly IBlobStorageService _blobStorageService;
+
+    public AddAttachmentCommandHandler(
+        IUnitOfWorkFactory unitOfWorkFactory,
+        IUserContext userContext,
+        IMapper mapper,
+        IBlobStorageService blobStorageService)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+        _userContext = userContext;
+        _mapper = mapper;
+        _blobStorageService = blobStorageService;
+    }
+
+    public async Task<AttachmentDto> Handle(
+        AddAttachmentCommand request,
+        CancellationToken cancellationToken)
+    {
+        var currentUserId = _userContext.UserId
+            ?? throw new UnauthorizedAccessException(
+                "User is not authenticated.");
+
+        using var uow = _unitOfWorkFactory.Create();
+
+        var card = await uow.CardRepository.GetByIdAsync(
+            request.CardId, cancellationToken)
+                ?? throw new NotFoundException(
+                    $"Card with ID {request.CardId} was not found.");
+
+        var section = await uow.SectionRepository.GetByIdAsync(
+            card.SectionId, cancellationToken)
+                ?? throw new NotFoundException(
+                    $"Section with ID {card.SectionId} was not found.");
+
+        var isCurrentMember = await uow.BoardMemberRepository.IsMemberAsync(
+            section.BoardId, currentUserId, cancellationToken);
+
+        if (!isCurrentMember)
+        {
+            throw new ForbiddenAccessException(
+                "You do not have access to add attachments to this card.");
+        }
+
+        var uploadedFileUrl = await _blobStorageService.UploadAsync(
+            request.FileStream,
+            request.FileName,
+            "attachments",
+            request.ContentType,
+            cancellationToken);
+
+        var attachment = new Attachment
+        {
+            Id = Guid.NewGuid(),
+            CardId = request.CardId,
+            FileUrl = uploadedFileUrl,
+            FileName = request.FileName,
+            FileSizeBytes = request.FileSizeBytes
+        };
+
+        try
+        {
+            await uow.AttachmentRepository.CreateAsync(
+                attachment, 
+                cancellationToken);
+
+            uow.Commit();
+        }
+        catch
+        {
+            uow.Rollback();
+            throw;
+        }
+
+        return _mapper.Map<AttachmentDto>(attachment);
+    }
+}
---
+++ WorkBoard.Application/Features/Attachments/Commands/AddAttachment/AddAttachmentCommandValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+
+namespace WorkBoard.Application.Features.Attachments.Commands.AddAttachment;
+
+public class AddAttachmentCommandValidator
+    : AbstractValidator<AddAttachmentCommand>
+{
+    public AddAttachmentCommandValidator()
+    {
+        RuleFor(x => x.FileName)
+            .NotEmpty()
+            .WithMessage("File name is required.");
+
+        RuleFor(x => x.ContentType)
+            .NotEmpty()
+            .WithMessage("Content type is required.");
+
+        RuleFor(x => x.FileSizeBytes)
+            .GreaterThan(0)
+            .WithMessage("File cannot be empty.")
+            .LessThanOrEqualTo(100 * 1024 * 1024)
+            .WithMessage("File size must not exceed 100 MB.");
+
+        RuleFor(x => x.FileStream)
+            .NotNull()
+            .WithMessage("File stream is required.");
+    }
+}
