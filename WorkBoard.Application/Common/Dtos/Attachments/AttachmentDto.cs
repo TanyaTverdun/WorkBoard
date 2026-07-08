c51f76707be9b85b622a134c39b58543eb554267
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-08 16:54:51 +0300

    feat: add AttachmentDto

---
+++ WorkBoard.Application/Common/Dtos/Attachments/AttachmentDto.cs
@@ -0,0 +1,14 @@
+﻿namespace WorkBoard.Application.Common.Dtos.Attachments;
+
+public class AttachmentDto
+{
+    public Guid Id { get; set; }
+
+    public Guid CardId { get; set; }
+
+    public required string FileUrl { get; set; }
+
+    public required string FileName { get; set; }
+
+    public long FileSizeBytes { get; set; }
+}
