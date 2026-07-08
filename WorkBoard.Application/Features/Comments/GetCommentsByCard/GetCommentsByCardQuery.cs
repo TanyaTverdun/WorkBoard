8fccba5e8bcfc67d3ab6faecc021e002ef5d6c18
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-08 11:35:38 +0300

    feat: add GetCommentsByCard query and handler

---
+++ WorkBoard.Application/Features/Comments/GetCommentsByCard/GetCommentsByCardQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using WorkBoard.Application.Common.Dtos.Comments;
+
+namespace WorkBoard.Application.Features.Comments.GetCommentsByCard;
+
+public record GetCommentsByCardQuery(Guid CardId)
+    : IRequest<IReadOnlyList<CommentDto>>;
