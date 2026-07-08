d5fee7d7fdc1ec2db07e0460c5163d2bc614311b
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-08 11:22:52 +0300

    feat: add Comment entity

---
+++ WorkBoard.Domain/Entities/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using WorkBoard.Domain.Common;
+
+namespace WorkBoard.Domain.Entities;
+
+[Table("Coments")]
+public class Comment : BaseEntity<Guid>
+{
+    [Column("ComentId")]
+    public override Guid Id { get; set; }
+
+    public required Guid CardId { get; set; }
+
+    public required Guid UserId { get; set; }
+
+    public required string Text { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+}
