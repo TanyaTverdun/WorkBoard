3d712eaa756021fc59cf9774d41f3179d96fe3c1
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-08 16:11:04 +0300

    feat: add UpdateCardDueDate endpoint to CardsController

--- WorkBoard.WebAPI/Controllers/CardsController.cs
+++ WorkBoard.WebAPI/Controllers/CardsController.cs
@@ -8,6 +8,7 @@ using WorkBoard.Application.Features.Cards.Commands.DeleteCard;
 using WorkBoard.Application.Features.Cards.Commands.DeleteCardAssignee;
 using WorkBoard.Application.Features.Cards.Commands.MoveCard;
 using WorkBoard.Application.Features.Cards.Commands.UpdateCardDescription;
+using WorkBoard.Application.Features.Cards.Commands.UpdateCardDueDate;
 using WorkBoard.Application.Features.Cards.Commands.UpdateCardTitle;
 using WorkBoard.Application.Features.Cards.Queries.GetAssignableUsers;
 using WorkBoard.Application.Features.Cards.Queries.GetCardAssignees;
@@ -516,6 +517,66 @@ public class CardsController : ControllerBase
         {
             CardId = cardId,
             TargetUserId = userId
+        };
+
+        await _mediator.Send(command, cancellationToken);
+
+        return NoContent();
+    }
+
+    /// <summary>
+    /// Updates the due date of a specific card
+    /// </summary>
+    /// <param name="boardId">
+    /// The unique identifier of the board containing the card
+    /// </param>
+    /// <param name="cardId">
+    /// The unique identifier of the card to update
+    /// </param>
+    /// <param name="request">
+    /// The object containing the new due date for the card
+    /// </param>
+    /// <param name="cancellationToken">
+    /// Cancellation token provided by the runtime
+    /// </param>
+    /// <returns>
+    /// No content upon successful update
+    /// </returns>
+    /// <response code="204">
+    /// Returns successfully with no content when the due date is updated
+    /// </response>
+    /// <response code="400">
+    /// If the input data is invalid
+    /// </response>
+    /// <response code="401">
+    /// If the user is not authenticated
+    /// </response>
+    /// <response code="403">
+    /// If the user does not have access to modify this card
+    /// </response>
+    /// <response code="404">
+    /// If the board or the card with the specified ID was not found
+    /// </response>
+    /// <response code="500">
+    /// If an internal server error occurs while processing the request
+    /// </response>
+    [HttpPut("/api/boards/{boardId:guid}/cards/{cardId:guid}/due-date")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> UpdateCardDueDate(
+        [FromRoute] Guid boardId,
+        [FromRoute] Guid cardId,
+        [FromBody] UpdateCardDueDateRequest request,
+        CancellationToken cancellationToken)
+    {
+        var command = new UpdateCardDueDateCommand
+        {
+            BoardId = boardId,
+            CardId = cardId,
+            DueDate = request.DueDate
         };
 
         await _mediator.Send(command, cancellationToken);
