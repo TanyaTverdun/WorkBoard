3f96adba19772f104bbf5f3021ebe010294188d2
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-09 17:24:19 +0300

    feat: add ActivityLogRepository and GetByCardIdAsync method

---
+++ WorkBoard.Persistence/Repositories/ActivityLogRepository.cs
@@ -0,0 +1,55 @@
+﻿using Dapper;
+using System.Data;
+using WorkBoard.Application.Common.Interfaces;
+using WorkBoard.Application.Common.Interfaces.Repositories;
+using WorkBoard.Domain.Entities;
+
+namespace WorkBoard.Persistence.Repositories;
+
+public class ActivityLogRepository
+    : GenericRepository<ActivityLog, Guid>, IActivityLogRepository
+{
+    public ActivityLogRepository(IDbConnectionFactory connectionFactory)
+        : base(connectionFactory)
+    {
+    }
+
+    internal ActivityLogRepository(
+        IDbConnection connection,
+        IDbTransaction transaction)
+        : base(connection, transaction)
+    {
+    }
+
+    public async Task<IReadOnlyList<ActivityLog>> GetByCardIdAsync(
+        Guid cardId,
+        CancellationToken cancellationToken = default)
+    {
+        const string sql = @"
+            SELECT 
+                ActivityLogId AS Id,
+                CardId,
+                UserId,
+                Text,
+                CreatedAt
+            FROM 
+                ActivityLogs
+            WHERE 
+                CardId = @CardId
+            ORDER BY 
+                CreatedAt DESC;";
+
+        var command = new CommandDefinition(
+            sql,
+            new
+            {
+                CardId = cardId
+            },
+            transaction: _transaction,
+            cancellationToken: cancellationToken);
+
+        var logs = await _connection.QueryAsync<ActivityLog>(command);
+
+        return logs.ToList().AsReadOnly();
+    }
+}
