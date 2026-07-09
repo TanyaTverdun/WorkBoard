50e593053f0672b3b90cba20bbcb2ee8446cf771
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-07-09 17:23:54 +0300

    feat: add ActivityLogDto

---
+++ WorkBoard.Application/Common/Dtos/ActivityLogs/ActivityLogDto.cs
@@ -0,0 +1,14 @@
+﻿namespace WorkBoard.Application.Common.Dtos.ActivityLogs;
+
+public class ActivityLogDto
+{
+    public Guid Id { get; set; }
+
+    public Guid CardId { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public string Text { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; }
+}
